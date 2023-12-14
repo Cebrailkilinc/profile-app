c10e8b1423434296dad60197fed33859581a3a7d
Cebrail Kilinc <cebrailyyy57@outlook.com>
2023-12-14 12:32:58 +0300

    update

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -28,32 +28,32 @@ const Detail = () => {
     const [detailControl, setDetailControl] = useState("education")
     return (
         <div>
-            <div className='flex items-start  justify-between  text-textBoldBlue pt-[2px] w-full' >
-                <div onClick={() => { setDetailControl("education") }} className='w-full ' >
+            <div className='flex flex-wrap items-start  justify-between  text-textBoldBlue pt-[2px] w-full' >
+                <div onClick={() => { setDetailControl("education") }} className=' ' >
                     <div className={`flex items-center justify-center w-full pb-1 ${detailControl === "education" ? "border-textBoldBlue border-b-[3px] " : "border-b pb-[5px]"}  gap-2 cursor-pointer`}>
                         <img width="20" height="20" src="https://img.icons8.com/ios-glyphs/30/student-male.png" alt="student-male" />
                         <h1 className='text-sm font-semibold '>Eğitim</h1>
                     </div>
                 </div>
-                <div onClick={() => { setDetailControl("resume") }} className='w-full  '>
+                <div onClick={() => { setDetailControl("resume") }} className='  '>
                     <div className={`flex items-center justify-center  w-full pb-1 border-b ${detailControl === "resume" ? "border-tertiaryBlue border-b-[3px]" : "border-b pb-[5px]"}  gap-2 cursor-pointer`}>
                         <img width="20" height="20" src="https://img.icons8.com/carbon-copy/100/resume.png" alt="resume" />
                         <h1 className='text-sm font-semibold '>Özgeçmiş</h1>
                     </div>
                 </div>
-                <div onClick={() => { setDetailControl("expertise") }} className=' w-full '>
+                <div onClick={() => { setDetailControl("expertise") }} className=' '>
                     <div className={`flex items-center justify-center  w-full pb-1 border-b ${detailControl === "expertise" ? "border-tertiaryBlue border-b-[3px]" : "border-b pb-[5px]"}  gap-2 cursor-pointer`}>
                         <img width="20" height="20" src="https://img.icons8.com/external-others-phat-plus/64/external-expertise-medical-and-healthcare-outline-others-phat-plus-17.png" alt="external-expertise-medical-and-healthcare-outline-others-phat-plus-17" />
                         <h1 className='text-sm font-semibold '>Uzmanlık</h1>
                     </div>
                 </div>
-                <div onClick={() => { setDetailControl("certificate") }} className='w-full  '>
+                <div onClick={() => { setDetailControl("certificate") }} className='  '>
                     <div className={`flex items-center justify-center  w-full pb-1 border-b ${detailControl === "certificate" ? "border-tertiaryBlue border-b-[3px]" : "border-b pb-[5px]"}  gap-2 cursor-pointer`}>
                         <PiCertificateBold className='' size={20} />
                         <h1 className='text-sm font-semibold '>Sertifikalar</h1>
                     </div>
                 </div>
-                <div onClick={() => { setDetailControl("evaluation") }} className='w-full'>
+                <div onClick={() => { setDetailControl("evaluation") }} className=''>
                     <div className={`flex items-center justify-center  w-full pb-1 border-b ${detailControl === "evaluation" ? "border-tertiaryBlue border-b-[3px]" : "border-b pb-[5px]"}  gap-2 cursor-pointer`}>
                         <img width="20" height="20" src="https://img.icons8.com/ios/50/test--v1.png" alt="test--v1" />
                         <h1 className='text-sm font-semibold '>Değerlendirme</h1>
