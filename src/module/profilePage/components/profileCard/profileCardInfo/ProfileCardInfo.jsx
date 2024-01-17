960d235acf7034d4f94442945b85a2af7693491d
Cebrail Kilinc <cebrailyyy57@outlook.com>
2024-01-17 17:39:25 +0300

    add option button and content

--- src/module/profilePage/components/profileCard/profileCardInfo/ProfileCardInfo.jsx
+++ src/module/profilePage/components/profileCard/profileCardInfo/ProfileCardInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useRef, useEffect } from 'react'
 //Components
 import Rating from "../../../../../package/components/content/Rating"
 // Icons
@@ -13,6 +13,7 @@ import { IoMdWallet } from "react-icons/io";
 import { CiHeart } from "react-icons/ci";
 import { GrLanguage } from "react-icons/gr";
 import { IoIosCloseCircleOutline } from "react-icons/io";
+import { SlOptionsVertical } from "react-icons/sl";
 //This props came from <ProfilePageLayout/>  component
 const ProfileCardHeader = (
     {
@@ -27,8 +28,12 @@ const ProfileCardHeader = (
         socialRef
     }) => {
     //Cebrail
+    //cebrail salak mısımn ne kadar konuşuyosun şuan ne saçma bir şey yapıyoruz salak mıyız yazıyom devamlı birşeyler ne yazasam neyse zeki görüneyim hadi by :)
+
+    const optionsRef = useRef()
 
     const [wideScreenImg, setWideScreenImg] = useState(false)
+    const [showOptions, setShowOptions] = useState(true)
 
     //This function updates the icon and opens <Evaluation/> in the profile detail field.
     const handleOpenCommentDetailPage = useCallback((event) => {
@@ -45,6 +50,21 @@ const ProfileCardHeader = (
             return
         }
     })
+
+    useEffect(() => {
+        const checkIfClickedOutside = e => {        
+          if (showOptions && optionsRef.current && !optionsRef.current.contains(e.target)) {
+            setShowOptions(false)
+          }
+        }
+    
+        document.addEventListener("mousedown", checkIfClickedOutside)
+    
+        return () => {
+          // Cleanup the event listener
+          document.removeEventListener("mousedown", checkIfClickedOutside)
+        }
+      }, [showOptions])
 
     return (
         <>
@@ -122,6 +142,22 @@ const ProfileCardHeader = (
                             </div>
                         )}
                         <BsBoxArrowUp onClick={() => { setIsFollow(!isFollow) }} className={`${isFollow ? "text-primaryBlue" : ""} hover:opacity-60`} />
+                        <div className='relative group z-50 '>
+                            <SlOptionsVertical onClick={() => setShowOptions(!showOptions)} className='group-hover:opacity-60 ' />
+                            <div ref={optionsRef} className={`absolute top-4 right-0 ${showOptions ? "flex" : "hidden"} flex-col w-32 pt-2`} >
+                                <div className='py-2  w-full p-3 border rounded-lg text-[10px] bg-lighBlue  text-black' >
+                                    <div className='flex items-center gap-2 text-xs'>
+                                        <img width="18" height="18" src="https://img.icons8.com/color/48/spam.png" alt="spam" />
+                                        <h1 className='hover:opacity-75' >Spam</h1>
+                                    </div>
+                                    <div className='flex items-center gap-2 text-xs'>
+                                        <img width="18" height="18" src="https://img.icons8.com/color/48/complaint.png" alt="complaint" />
+                                        <h1 className='hover:opacity-75'>Şikayet Et</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                     <div className=' w-full flex gap-3 items-center justify-end cursor-pointer'>
                         <div className='flex items-center gap-1 mb-1 ' onClick={handleOpenCommentDetailPage} >
@@ -166,14 +202,14 @@ const ProfileCardHeader = (
             </div>
             {wideScreenImg && (
                 <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50'>
-                   
-                        <IoIosCloseCircleOutline onClick={() => setWideScreenImg(false)} size={35} className='absolute top-20 tablet:top-28 right-4 text-white hover:opacity-80 cursor-pointer' />
-                        <img
-                            src="/profileImage.jpg"
-                            className={`rounded-full w-4/5 p-2 telefon:w-3/5 miniTablet:w-2/5 tablet:w-auto tablet:h-2/3`}
-                            alt=""
-                        />
-           
+
+                    <IoIosCloseCircleOutline onClick={() => setWideScreenImg(false)} size={35} className='absolute top-20 tablet:top-28 right-4 text-white hover:opacity-80 cursor-pointer' />
+                    <img
+                        src="/profileImage.jpg"
+                        className={`rounded-full w-4/5 p-2 telefon:w-3/5 miniTablet:w-2/5 tablet:w-auto tablet:h-2/3`}
+                        alt=""
+                    />
+
                 </div>
             )}
 
